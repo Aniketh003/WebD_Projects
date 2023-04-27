535f0acbd61a68fa087b74050f0b3e3445d1b404
Aniketh003 <21wj1a6912@gniindia.org>
2023-04-28 01:13:35 +0530

    Todo Added

---
+++ Random Feed/script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.container')
+const unsplashURL = 'https://source.unsplash.com/random?film,4k';
+const rows = 10
+
+for(let i=0;i<rows*3;i++){
+    const img = document.createElement('img')
+    img.src = `${unsplashURL}${randomsize()}`
+    container.appendChild(img)
+}
+function randomsize(){
+    return `${getRandom()}x${getRandom()}`
+}
+function getRandom(){
+    return Math.floor(Math.random() * 10) + 300
+}
