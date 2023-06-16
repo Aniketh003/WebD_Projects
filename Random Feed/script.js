9ae4b9c70a2627c7aa03cbc5559a4a9322d3f759
Aniketh <21wj1a6912@gniindia.org>
2023-06-17 00:18:14 +0530

    Update script.js

--- Random Feed/script.js
+++ Random Feed/script.js
@@ -4,6 +4,7 @@ const rows = 10
 
 for(let i=0;i<rows*3;i++){
     const img = document.createElement('img')
+    img.setAttribute('loading', 'lazy');
     img.src = `${unsplashURL}${randomsize()}`
     container.appendChild(img)
 }
